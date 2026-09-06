8d6af1c5bc8700e19d64e19a408aedfd947caa45
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2026-09-06 15:24:45 -0400

    Preserve UDP listener identity during callbacks

--- UDPsocket.cpp
+++ UDPsocket.cpp
@@ -18,6 +18,10 @@ static char THIS_FILE[] = __FILE__;
 UDPsocket::UDPsocket(CMUSHclientDoc * pDoc)
 {
 m_pDoc = pDoc;
+m_iPluginInstanceNumber = 0;
+m_bInReceive = false;
+m_bReceivePending = false;
+m_bDeleteWhenDone = false;
 
 }
 
@@ -39,6 +43,40 @@ END_MESSAGE_MAP()
 
 void UDPsocket::OnReceive(int nErrorCode)
   {
+  if (m_bInReceive)
+    {
+    m_bReceivePending = true;
+    return;
+    }
+
+  m_bInReceive = true;
+  try
+    {
+    do
+      {
+      m_bReceivePending = false;
+      ReceiveOneDatagram ();
+      } while (m_bReceivePending && !m_bDeleteWhenDone);
+    }
+  catch (...)
+    {
+    bool bDeleteWhenDone = m_bDeleteWhenDone;
+    m_bInReceive = false;
+    m_bReceivePending = false;
+    if (bDeleteWhenDone)
+      delete this;
+    throw;
+    }
+
+  bool bDeleteWhenDone = m_bDeleteWhenDone;
+  m_bInReceive = false;
+  m_bReceivePending = false;
+  if (bDeleteWhenDone)
+    delete this;
+  }
+
+void UDPsocket::ReceiveOneDatagram (void)
+  {
   char buff [1000];
   int count = Receive (buff, sizeof (buff) - 1);
 
@@ -48,6 +86,8 @@ void UDPsocket::OnReceive(int nErrorCode)
   buff [count] = 0;   // null terminate it
 
   // call script
-  m_pDoc->CallPlugin (m_strPluginID, m_strScript, buff);
+  if (m_pDoc->GetPluginInstance (m_strPluginID,
+                                 m_iPluginInstanceNumber))
+    m_pDoc->CallPlugin (m_strPluginID, m_strScript, buff);
 
   }
