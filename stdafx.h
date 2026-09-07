5095e3d62b14b0cb2ee6b98765e9a1654b22e42a
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2026-09-06 22:08:41 -0400

    Keep unrelated header declarations unchanged

--- stdafx.h
+++ stdafx.h
@@ -86,6 +86,7 @@
 #include <map>
 #include <set>
 #include <list>
+#include <memory>
 #include <algorithm>
 #include <functional>
 #include <iostream>
@@ -368,25 +369,6 @@ CString FixupEscapeSequences (const CString & strSource);
 void SetFileDialogFileName (CFileDialog & dialog, CString & buffer,
                             const CString & initialName);
 
-class CBoolStateGuard
-  {
-  public:
-  CBoolStateGuard (bool & bValue, const bool bNewValue)
-    : m_bValue (bValue), m_bSavedValue (bValue)
-    {
-    m_bValue = bNewValue;
-    }
-
-  ~CBoolStateGuard ()
-    {
-    m_bValue = m_bSavedValue;
-    }
-
-  private:
-  bool & m_bValue;
-  bool m_bSavedValue;
-  };
-
 template <class T>
 class CValueStateGuard
   {
@@ -411,6 +393,25 @@ class CValueStateGuard
 
 // translates "send to" numbers
 CString GetSendToString (const unsigned short iWhere);
+
+class CBoolStateGuard
+  {
+  public:
+  CBoolStateGuard (bool & bValue, const bool bNewValue)
+    : m_bValue (bValue), m_bSavedValue (bValue)
+    {
+    m_bValue = bNewValue;
+    }
+
+  ~CBoolStateGuard ()
+    {
+    m_bValue = m_bSavedValue;
+    }
+
+  private:
+  bool & m_bValue;
+  bool m_bSavedValue;
+  };
 
 // translates connection status to text
 CString GetConnectionStatus (const int iStatus);
